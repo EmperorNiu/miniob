ebadcb9e08a3fe4228fb03afe83bc3aef0fbd459
Niu Yuean <niuyuean99@sina.com>
2021-11-18 20:39:45 +0800

    fix multi-index

--- src/observer/storage/common/condition_filter.cpp
+++ src/observer/storage/common/condition_filter.cpp
@@ -187,9 +187,9 @@ bool DefaultConditionFilter::filter(const Record &rec) const
     case FLOATS: {
       float left = *(float *)left_value;
       float right = *(float *)right_value;
-      if (right-left == 0)cmp_result =0;
-      if (right-left > 0)cmp_result =1;
-      if (right-left < 0)cmp_result =-1;
+      if (left-right == 0)cmp_result =0;
+      if (left-right > 0)cmp_result =1;
+      if (left-right < 0)cmp_result =-1;
       // cmp_result = (int)(left - right);
     } break;
     case DATES:{
