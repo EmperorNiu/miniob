9359285e65c804d29f893c8e9f6ad5841e770f84
Niu Yuean <niuyuean99@sina.com>
2021-11-19 11:18:58 +0800

    fix multi-index

--- src/observer/storage/common/table.cpp
+++ src/observer/storage/common/table.cpp
@@ -1092,11 +1092,12 @@ IndexScanner *Table::find_index_for_scan(const ConditionFilter *filter) {
   for (int i = 0; i < indexes_.size(); ++i) {
     if(indexes_[i]->field_metas().size() >1){
       flag = 1;
+      break;
     }
   }
   IndexScanner *scanner;
-  if (flag == 0) scanner= find_index_for_scan2(filter);
-  else scanner= find_index_for_scan_multi(filter);
+  if (flag == 1) scanner=find_index_for_scan2(filter);
+  else scanner=find_index_for_scan_multi(filter);
   if (scanner != nullptr) {
     return scanner;
   }
